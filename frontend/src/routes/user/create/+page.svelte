27aa2fc7d558fe84349b8158e4e344651b875e2c
SaltyShaman <kristoffer6400@gmail.com>
2026-02-13 15:55:31 +0100

    Create user page styling

--- frontend/src/routes/user/create/+page.svelte
+++ frontend/src/routes/user/create/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import "./create-user.css";
+
     let username = "";
     let password = "";
     let errorMessage = "";
@@ -21,20 +23,19 @@
             const data = await response.json();
 
             if (!response.ok) {
-                // Show backend errors (username taken, etc.)
                 errorMessage = data.message || data.error || "Failed to create user";
                 return;
             }
 
             successMessage = `User "${username}" created successfully! Redirecting to login page...`;
 
-            // Reset form
             username = "";
             password = "";
 
             setTimeout(() => {
                 window.location.href = "/login";
-            }, 3000); // 3 seconds
+            }, 3000);
+
         } catch (err) {
             console.error(err);
             errorMessage = "An error occurred while creating the user";
@@ -42,28 +43,30 @@
     }
 </script>
 
-<main>
-    <h1>Create User</h1>
+<main class="create-user-page">
+    <div class="create-user-card">
+        <h1>Create User</h1>
 
-    <form on:submit={handleCreateUser}>
-        <div>
-            <label for="username">Username:</label>
-            <input id="username" bind:value={username} required />
-        </div>
+        <form on:submit={handleCreateUser}>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input id="username" bind:value={username} required />
+            </div>
 
-        <div>
-            <label for="password">Password:</label>
-            <input id="password" type="password" bind:value={password} required />
-        </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input id="password" type="password" bind:value={password} required />
+            </div>
 
-        {#if errorMessage}
-            <p style="color:red">{errorMessage}</p>
-        {/if}
+            {#if errorMessage}
+                <p class="error">{errorMessage}</p>
+            {/if}
 
-        {#if successMessage}
-            <p style="color:green">{successMessage}</p>
-        {/if}
+            {#if successMessage}
+                <p class="success">{successMessage}</p>
+            {/if}
 
-        <button type="submit">Create User</button>
-    </form>
+            <button type="submit">Create Account</button>
+        </form>
+    </div>
 </main>
